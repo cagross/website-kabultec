e9d74a3305e53f02ac69cfd839846e69b67db50d
Carl Gross <admin@kabultec.org>
2020-05-26 06:24:04 -0700

    Ensure radio button value is preset donation amount in PayPal.

--- assets/js/script-donate.js
+++ assets/js/script-donate.js
@@ -1,51 +1,76 @@
-console.log(555);
+// console.log(555);
 
 
 
-document.getElementById("myBtn").onclick = displayDate;
+// document.getElementById("myBtn").onclick = displayDate;
 
-function displayDate() {
-  document.getElementById("demo").innerHTML = Date();
-}
+// function displayDate() {
+//   document.getElementById("demo").innerHTML = Date();
+// }
 
 // function CalculateOrder(form) {
-// "use strict";
-// 	var radG1;
+function CalculateOrder(myform) {
 
-// 	for (var i = 0; i < document.myform.os0.length; i++){
-// 	 if(document.myform.os0[i].checked==true){radG1=document.myform.os0[i].value;}
-// 	}
+  // console.log(555);
+  // alert(555);
 
-// 	console.log(radG1);
+  
+  "use strict";
+	var radG1;
 
-// 	if(radG1 == "10")
-// 	 {
-// 	 document.myform.amount.value = '10';
-// 	 document.myform.item_number.value = "WF-10-00";
-// 	 }
-// 	else if(radG1 == "500")
-// 	 {
-// 	 document.myform.amount.value = '500.00';
-// 	 document.myform.item_number.value = "WF-500-00";
-// 	 }
-// 	 else if(radG1 == "100")
-// 	 {
-// 	 document.myform.amount.value = '100.00';
-// 	 document.myform.item_number.value = "WF-100-00";
-// 	 }
-// 	 else if(radG1 == "50")
-// 	 {
-// 	 document.myform.amount.value = '50.00';
-// 	 document.myform.item_number.value = "WF-50-00";
-// 	 }
-// 	 else if(radG1 == "25")
-// 	 {
-// 	 document.myform.amount.value = '25.00';
-// 	 document.myform.item_number.value = "WF-25-00";
-// 	 }
-// 	else if(radG1 == "Other")
-// 	 {
-// 	 document.myform.amount.value = '0.00';
-// 	 document.myform.item_number.value = "WF-0-00";
-// 	 }
-// }
+	// i = 0; i < document.myform.os0.length; i++){
+	//  nt.myform.os0[i].checked==true){radG1=document.myform.os0[i].value;}
+  // }
+  const els = myform.getElementsByClassName('raddy')
+  // for (var i = 0; i < myform.os0.length; i++){
+  for (var i = 0; i < els.length; i++){
+
+    // if(myform.os0[i].checked==true){
+    if(els[i].checked==true){
+
+      // radG1 = myform.os0[i].value;
+      // alert('Starting');
+      // myform.amount.value = myform.os0[i].value;
+      myform.amount.value = els[i].value;
+      return;
+
+    }
+  }
+
+	// console.log(radG1);
+
+	// if(radG1 == "10")
+	//  {
+  //  myform.amount.value = '10';
+  // myform.amount.value = '10';
+  
+	//  myform.item_number.value = "WF-10-00";
+	//  }
+	// else if(radG1 == "500")
+	//  {
+	//  myform.amount.value = '500.00';
+	//  myform.item_number.value = "WF-500-00";
+	//  }
+	//  else if(radG1 == "100")
+	//  {
+	//  myform.amount.value = '100.00';
+	//  myform.item_number.value = "WF-100-00";
+	//  }
+	//  else if(radG1 == "50")
+	//  {
+	//  myform.amount.value = '50.00';
+	//  myform.item_number.value = "WF-50-00";
+	//  }
+	//  else if(radG1 == "25")
+	//  {
+	//  myform.amount.value = '25.00';
+	//  myform.item_number.value = "WF-25-00";
+	//  }
+	// else if(radG1 == "Other")
+	//  {
+	//  myform.amount.value = '0.00';
+	//  myform.item_number.value = "WF-0-00";
+	//  }
+
+
+  }
