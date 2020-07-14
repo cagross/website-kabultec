a9c4f1a55084c254e779d2bb3aa17f389dbe00dc
Carl Gross <admin@kabultec.org>
2020-07-14 02:55:24 -0700

    Troubleshoot issue with 'this' keyword in donate form.

--- assets/js/script-donate.js
+++ assets/js/script-donate.js
@@ -1,13 +1,62 @@
 "use strict";
 
+
+// object.addEventListener("submit", myScript);
+
+
+const buttons = document.querySelectorAll('.kt-don-row-but button')
+
+
+
+
+
+
+function testy() {
+  console.log('Clicked element is:');
+  console.log(this);
+
+  // let donAmt = 555;
+  // const myForm = document.getElementById("myForm");
+
+  // if (this.querySelector('div > .donButCust')) {// If the hovered card has an <input> element, set donAmt to the value of that element.
+  //   donAmt = document.querySelector('.kt-don-cust-el input').value
+  //   console.log('equal')
+  //   console.log(donAmt)
+
+  // } else {
+  //   donAmt = this.dataset.amt
+  // }
+  // myForm.amount.value = donAmt;
+}
+document.getElementById('myForm').addEventListener("submit", testy);
+
+
+for (i = 0; i < buttons.length; i++) {
+  // buttons[i].onclick = testy;
+  // console.log(buttons[i])
+  // buttons[i].addEventListener("click", testy);
+}
+// document.querySelector('#kt-amt-cust button').onclick = testy;
+// const document.querySelectorAll('#kt-amt-cust button')
+// .onclick = testy;
+
+
+
+
+
+
+
+
+
+
 /* Set the value of the data-amt attribute on each donation card.  Used by <form> to insert the donation amount upon submission. */
 const amounts = []
-  amounts['a'] = 10
-  amounts['b'] = 50
-  amounts['c'] = 100
+amounts['a'] = 10
+amounts['b'] = 50
+amounts['c'] = 100
 
 for (let key in amounts) {
-  let amtId = `kt-amt-${key}`  
+  let amtId = `kt-amt-${key}`
   let amtEl = document.getElementById(amtId)
   amtEl.setAttribute("data-amt", `${amounts[key]}`);
 }
@@ -32,7 +81,7 @@ function myScriptLeave() {
 
 const myEls = document.querySelectorAll('.kt-amt');
 
-for (var i = 0; i < myEls.length; i++){
+for (var i = 0; i < myEls.length; i++) {
   // myEls[i].onclick = myFunc;
   // myEls[i].addEventListener("mouseenter", myScript);
   // myEls[i].addEventListener("mouseleave", myScript);
@@ -41,7 +90,6 @@ for (var i = 0; i < myEls.length; i++){
 }
 
 // const buttons = document.querySelectorAll('.kt-don-row-but .donBut')
-const buttons = document.querySelectorAll('.kt-don-row-but button')
 
 
 
@@ -67,28 +115,28 @@ function myFunc() {
 
   //Loop through all donation cards, one by one.
   // for (var i = 0; i < donateCardElements.length; i++){
-    // If this card is not already highlighted, AND if this card is not the card that has been clicked (or mouse entered/left), then ignore it and cycle to the next iteration in the loop.
-    // if (!donateCardElements[i].classList.contains(classHighlighted) && donateCardElements[i] !== this) {continue}
-    // // If this card is the card that was clicked, OR it was already highlighted, toggle the highlighted class on it, and ensure the correct button is displayed on it.
-    // donateCardElements[i].classList.toggle(classHighlighted);
-    // const buttons = donateCardElements[i].querySelectorAll('.kt-don-row-but .donBut')
-    // for (let j = 0; j < buttons.length; j++){
-    //   buttons[j].classList.toggle('kt-hidden');
-    // }
-    // if (!donateCardElements[i].classList.contains(classHighlighted) && donateCardElements[i] !== this) {continue}
-    // If this card is the card that was clicked, OR it was already highlighted, toggle the highlighted class on it, and ensure the correct button is displayed on it.
-    this.classList.toggle(classHighlighted);
-    const buttonsCard = this.querySelectorAll('.kt-don-row-but .donBut')
-    // console.log(buttonsCard)
-    for (let j = 0; j < buttonsCard.length; j++){
-      buttonsCard[j].classList.toggle('kt-hidden');
-    }
+  // If this card is not already highlighted, AND if this card is not the card that has been clicked (or mouse entered/left), then ignore it and cycle to the next iteration in the loop.
+  // if (!donateCardElements[i].classList.contains(classHighlighted) && donateCardElements[i] !== this) {continue}
+  // // If this card is the card that was clicked, OR it was already highlighted, toggle the highlighted class on it, and ensure the correct button is displayed on it.
+  // donateCardElements[i].classList.toggle(classHighlighted);
+  // const buttons = donateCardElements[i].querySelectorAll('.kt-don-row-but .donBut')
+  // for (let j = 0; j < buttons.length; j++){
+  //   buttons[j].classList.toggle('kt-hidden');
   // }
- 
-  
+  // if (!donateCardElements[i].classList.contains(classHighlighted) && donateCardElements[i] !== this) {continue}
+  // If this card is the card that was clicked, OR it was already highlighted, toggle the highlighted class on it, and ensure the correct button is displayed on it.
+  this.classList.toggle(classHighlighted);
+  const buttonsCard = this.querySelectorAll('.kt-don-row-but .donBut')
+  // console.log(buttonsCard)
+  for (let j = 0; j < buttonsCard.length; j++) {
+    buttonsCard[j].classList.toggle('kt-hidden');
+  }
+  // }
+
+
 
   //Set the forms amount value to the value of the clicked card's data-amt attribute.
-  
+
 
 
   // if (this.querySelector('.kt-don-cust-el input')) {// If the hovered card has an <input> element, set donAmt to the value of that element.
@@ -103,32 +151,6 @@ function myFunc() {
 
 
 }
-
-function testy() {
-  console.log('testy');
-  console.log(this);
-
-  // let donAmt = 555;
-  // const myForm = document.getElementById("myForm");
-
-  // if (this.querySelector('div > .donButCust')) {// If the hovered card has an <input> element, set donAmt to the value of that element.
-  //   donAmt = document.querySelector('.kt-don-cust-el input').value
-  //   console.log('equal')
-  //   console.log(donAmt)
-
-  // } else {
-  //   donAmt = this.dataset.amt
-  // }
-  // myForm.amount.value = donAmt;
-}
-for (i = 0; i < buttons.length; i++){
-  // buttons[i].onclick = testy;
-  console.log(buttons[i])
-  buttons[i].addEventListener("click", testy);
-}
-// document.querySelector('#kt-amt-cust button').onclick = testy;
-// const document.querySelectorAll('#kt-amt-cust button')
-// .onclick = testy;
 
 
 
@@ -159,43 +181,42 @@ function calculateOrder() {
 
 
 
-	// var radG1;
-	// console.log(radG1);
+  // var radG1;
+  // console.log(radG1);
 
-	// if(radG1 == "10")
-	//  {
+  // if(radG1 == "10")
+  //  {
   //  myform.amount.value = '10';
   // myform.amount.value = '10';
-  
-	//  myform.item_number.value = "WF-10-00";
-	//  }
-	// else if(radG1 == "500")
-	//  {
-	//  myform.amount.value = '500.00';
-	//  myform.item_number.value = "WF-500-00";
-	//  }
-	//  else if(radG1 == "100")
-	//  {
-	//  myform.amount.value = '100.00';
-	//  myform.item_number.value = "WF-100-00";
-	//  }
-	//  else if(radG1 == "50")
-	//  {
-	//  myform.amount.value = '50.00';
-	//  myform.item_number.value = "WF-50-00";
-	//  }
-	//  else if(radG1 == "25")
-	//  {
-	//  myform.amount.value = '25.00';
-	//  myform.item_number.value = "WF-25-00";
-	//  }
-	// else if(radG1 == "Other")
-	//  {
-	//  myform.amount.value = '0.00';
-	//  myform.item_number.value = "WF-0-00";
-	//  }
+
+  //  myform.item_number.value = "WF-10-00";
+  //  }
+  // else if(radG1 == "500")
+  //  {
+  //  myform.amount.value = '500.00';
+  //  myform.item_number.value = "WF-500-00";
+  //  }
+  //  else if(radG1 == "100")
+  //  {
+  //  myform.amount.value = '100.00';
+  //  myform.item_number.value = "WF-100-00";
+  //  }
+  //  else if(radG1 == "50")
+  //  {
+  //  myform.amount.value = '50.00';
+  //  myform.item_number.value = "WF-50-00";
+  //  }
+  //  else if(radG1 == "25")
+  //  {
+  //  myform.amount.value = '25.00';
+  //  myform.item_number.value = "WF-25-00";
+  //  }
+  // else if(radG1 == "Other")
+  //  {
+  //  myform.amount.value = '0.00';
+  //  myform.item_number.value = "WF-0-00";
+  //  }
 
 
-  }
+}
 
-  
