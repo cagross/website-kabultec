004cbe1e969110d0ffb59d33eda540b1585806cb
Carl Gross <admin@kabultec.org>
2023-06-24 21:00:47 -0700

    Migrate Google Analytics to G4.

--- functions.php
+++ functions.php
@@ -67,21 +67,6 @@ function spacious_render_header_image() {
     endif;
 /****************************************************************************************/
 
-// add_action('wp_footer', 'add_googleanalytics');
-function add_googleanalytics() {
-?>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-90010913-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
-<?php }
-
 // add_action('wp_footer', 'add_googleanalytics_4');
 function add_googleanalytics_4() {
   ?>
